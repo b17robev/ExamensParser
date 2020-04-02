8c73a77e13fad72d89578311a20f2a8a09ad82df
b17robev <b17robev@student.his.se>
2020-04-02 09:16:26 +0200

    added datasets + airportParser

---
+++ airports.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+    <?php
+
+    $file = fopen("data/airports.csv", "r");
+    try {
+        $conn = mysqli_connect("localhost","root","admin","examensarbete");
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
+
+    $conn->set_charset("UTF-8");
+
+    // each line into the local $data variable
+    $count = 0;
+    $flag = true;
+    while (($data = fgetcsv($file, 9999, ",")) !== FALSE)
+    {
+        if($flag) {
+            $flag = false;
+            continue;
+        }
+
+        $res = array_map("utf8_decode", $data) ;
+
+        $sql = "INSERT INTO airports (airport_id, name, city, country, iata, icao, latitude, longitude, altitude, timezone, dst, tz_database_time_zone, type, source, location) 
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        try {
+            if($stmt = $conn->prepare($sql)) {
+                $stmt->bind_param("isssssddissssss", $res[0],$res[1],$res[2],$res[3],$res[4],$res[5],$res[6],$res[7],$res[8],$res[9],$res[10],$res[11],$res[12],$res[13],$res[14]);
+                $stmt->execute();
+                if(!$stmt->execute()) {
+                    echo "Error: " . mysqli_error($conn);
+                }
+                $count++;
+            } else {
+                echo $conn->error;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+    }
+    fclose($file);
+    $conn->close();
+
+    echo "Data parsed successfully, added $count airports";
+    ?>
+    </body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form method="post" action="airports.php">
+            <h3>Parse Airports into Database</h3>
+            <button type="submit">Add</button>
+        </form>
+    </body>
+</html>
